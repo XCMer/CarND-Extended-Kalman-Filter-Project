2ffe8613bf4ed9ced7659a06d3d4cc5e0fbb41b9
Raahul Seshadri <raahul@musictypes.com>
2017-11-11 13:28:29 +0530

    Optimizations

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -52,7 +52,17 @@ FusionEKF::FusionEKF() {
   ekf_.P_ << 1,    0,    0,    0,
              0,    1,    0,    0,
              0,    0, 1000,    0,
-             0,    0,    0, 1000;;
+             0,    0,    0, 1000;
+
+  // State transition
+  ekf_.F_ = MatrixXd(4, 4);
+  ekf_.F_ << 1, 0, 1, 0,
+             0, 1, 0, 1,
+             0, 0, 1, 0,
+             0, 0, 0, 1;
+
+  // Initialize tools
+  tools = Tools();
 }
 
 /**
@@ -82,7 +92,6 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       float m_x = m_rho * cos(m_phi);
       float m_y = m_rho * sin(m_phi);
 
-
       ekf_.x_[0] = m_x;
       ekf_.x_[1] = m_y;
       ekf_.x_[2] = 0;
@@ -97,9 +106,11 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       ekf_.x_[3] = 0;
     }
 
+    // Set the previous timestamp
+    previous_timestamp_ = measurement_pack.timestamp_;
+
     // done initializing, no need to predict or update
     is_initialized_ = true;
-    previous_timestamp_ = measurement_pack.timestamp_;
     return;
   }
 
@@ -112,17 +123,16 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    * 1. The time_delta, which is the time in seconds since the last measurement
    * 2. Using the time_delta, we generate a state transition matrix. This is set
    *    inside the Kalman filter before calling Predict.
+   * 3. We also calculate the process covariance (Q)
    */
   float time_delta = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
   previous_timestamp_ = measurement_pack.timestamp_;
 
-  MatrixXd F_ = MatrixXd(4, 4);
-  F_ << 1, 0, time_delta, 0,
-        0, 1,          0, time_delta,
-        0, 0,          1, 0,
-        0, 0,          0, 1;
-  ekf_.F_ = F_;
+  // Update state transition matrix
+  ekf_.F_(0, 2) = time_delta;
+  ekf_.F_(1, 3) = time_delta;
 
+  // Calculate process covariance
   float ax2 = 9;
   float ay2 = 9;
   float t2 = time_delta * time_delta;
@@ -141,16 +151,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   /*****************************************************************************
    *  Update
    ****************************************************************************/
-
-  /**
-   TODO:
-     * Use the sensor type to perform the update step.
-     * Update the state and covariance matrices.
-   */
-
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-    Tools tools = Tools();
-
+    // Radar Updates
     ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
     ekf_.R_ = R_radar_;
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -30,34 +30,41 @@ void KalmanFilter::Predict() {
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
+  // Calculate the measurement delta
   VectorXd y = z - H_ * x_;
+
+  // Calculate Kalman gain
   MatrixXd Ht = H_.transpose();
   MatrixXd S = H_ * P_ * Ht + R_;
   MatrixXd Si = S.inverse();
   MatrixXd K = P_ * Ht * Si;
 
+  // Update state and its covariance
   x_ = x_ + (K * y);
   P_ = (MatrixXd::Identity(4, 4) - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  // Convert state to polar co-ordinates
   VectorXd hx_(3);
   float x_rms = sqrt(x_[0]*x_[0] + x_[1]*x_[1]);
   hx_ << x_rms, atan2(x_[1], x_[0]), (x_[0]*x_[2] + x_[1]*x_[3]) / x_rms;
 
-  // Update
+  // Calculate the measurement delta
   VectorXd y = z - hx_;
 
-  // Reference: Normalize phi to -pi to pi after subtraction
-  // https://discussions.udacity.com/t/already-used-atan2-to-calculate-phi-in-hx-do-i-still-need-to-normalize-the-phi-in-y/242332/4
+  // Normalize phi to -pi to pi after subtraction
+  // Reference: https://discussions.udacity.com/t/already-used-atan2-to-calculate-phi-in-hx-do-i-still-need-to-normalize-the-phi-in-y/242332/4
+  // We're basically doing atan(tan(y[1])). However, atan normalizes to between -pi and pi
   y[1] = atan2(sin(y[1]), cos(y[1]));
-  std::cout<<"!!!!!"<<y<<"!!!!!"<<std::endl;
 
+  // Calculate K
   MatrixXd Hjt = H_.transpose();
   MatrixXd S = H_ * P_ * Hjt + R_;
   MatrixXd Si = S.inverse();
   MatrixXd K = P_ * Hjt * Si;
 
+  // Update state and its covariance
   x_ = x_ + (K * y);
   P_ = (MatrixXd::Identity(4, 4) - K * H_) * P_;
 }
